09ca993141778ce4afc1d1953dec6d2d00556222
luciana.lucio <luciana.lucio@g.globo>
2024-02-15 20:26:10 -0300

    Aula 01

---
+++ 001-inicio/index.js
@@ -0,0 +1,14 @@
+// Typescript
+// npm install -g typescript
+// tsc -v
+// tsc --init
+// Plugin: Code Runner
+// npm i -g ts-node
+// ctrl + opt + n => para o Code Runner rodar
+// tsc index.ts => converte para um arquivo .js
+
+console.log("Hello World!");
+var pedido = function (pedido) {
+    return "Deu bom o seu pedido ".concat(pedido);
+};
+console.log(pedido(123));
