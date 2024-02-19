3321c50cf80f447a9194a62b0d46668b0bad073d
luciana.lucio <luciana.lucio@g.globo>
2024-02-19 20:02:58 -0300

    Aula 02 - parte 07

---
+++ 002-tipos/007-aliases/index.ts
@@ -0,0 +1,12 @@
+type alphanumeric = string | number;
+
+let dados: alphanumeric;
+let dados2: alphanumeric;
+
+dados = 1;
+dados = "Luciana";
+//dados = false;
+
+dados2 = 1;
+dados2 = "Luciana";
+//dados2 = false;
